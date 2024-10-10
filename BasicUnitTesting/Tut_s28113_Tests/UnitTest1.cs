4a554eb32e006fe9910b874139d666d694b1b1b1
Piotr Dancewicz <s28113@pjwstk.edu.pl>
2024-10-10 15:13:20 +0200

    Added unit tests for Sphere

--- BasicUnitTesting/Tut_s28113_Tests/UnitTest1.cs
+++ BasicUnitTesting/Tut_s28113_Tests/UnitTest1.cs
@@ -1,15 +1,21 @@
+using BasicUnitTesting;
+
 namespace Tut_s28113_Tests;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
+
+    private readonly IShape sphere = new Sphere(5);
+
+    [Test]
+    public void TestSphereCalculateArea()
     {
+        Assert.That(sphere.CalculateArea(), Is.EqualTo(314.159).Within(0.001));
     }
 
     [Test]
-    public void Test1()
+    public void TestSphereCalculateVolume()
     {
-        Assert.Pass();
+        Assert.That(sphere.CalculateVolume(), Is.EqualTo(523.598).Within(0.001));
     }
 }
