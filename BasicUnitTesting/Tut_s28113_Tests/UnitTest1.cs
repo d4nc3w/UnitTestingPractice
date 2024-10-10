0a9a98fd410c4071546fecc56de4afa987de428c
Piotr Dancewicz <s28113@pjwstk.edu.pl>
2024-10-10 15:08:11 +0200

    Added UnitTests Project

---
+++ BasicUnitTesting/Tut_s28113_Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Tut_s28113_Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
