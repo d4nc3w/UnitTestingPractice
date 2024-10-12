f9244d43e31aa94229efd6dcbd479fbefdb1ca60
Piotr Dancewicz <s28113@pjwstk.edu.pl>
2024-10-12 17:24:24 +0200

    Added False tests and division test

--- SimpleCalclator/UnitTests/CalculatorTests.cs
+++ SimpleCalclator/UnitTests/CalculatorTests.cs
@@ -16,17 +16,31 @@ public class Tests
     public void TestAddOperation()
     {
         Assert.That(_calculator.calculate(5, 5, '+'), Is.EqualTo(10));
+        //False
+        Assert.That(_calculator.calculate(5, 5, '+'), Is.EqualTo(5));
     }
 
     [Test]
     public void TestSubtractOperation()
     {
         Assert.That(_calculator.calculate(100, 75, '-'), Is.EqualTo(25));
+        //False
+        Assert.That(_calculator.calculate(100, 75, '-'), Is.EqualTo(50));
     }
 
     [Test]
     public void TestMultiplyOperation()
     {
         Assert.That(_calculator.calculate(37, 5, '*'), Is.EqualTo(185));
+        //False
+        Assert.That(_calculator.calculate(37, 5, '*'), Is.EqualTo(190));
+    }
+
+    [Test]
+    public void TestDivideOperation()
+    {
+        Assert.That(_calculator.calculate(66, 6, '/'), Is.EqualTo(11));
+        //False
+        Assert.That(_calculator.calculate(10, 0, '/'), Is.EqualTo(0));
     }
 }
